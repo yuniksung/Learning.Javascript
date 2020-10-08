7fd62533f7f828f14c0f106706df2cfad5671f51
Joshua Sung <yuniksung@gmail.com>
2020-10-08 02:45:33 -0500

    shot-input success

--- assets/js/game.js
+++ assets/js/game.js
@@ -127,17 +127,15 @@ var randomNumber = function(min, max) {
 var shop = function() {
   var shopOptionPrompt = window.prompt("Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice");
 // use switch to carry out action
+shopOptionPrompt = parseInt(shopOptionPrompt);
 switch (shopOptionPrompt) {
-  case "REFILL":
-  case "refill":
+  case 1:
       playerInfo.refillHealth();
       break;
-  case "UPGRADE":
-  case "upgrade":
+  case 2:
       playerInfo.upgradeAttack();
       break;
-  case "LEAVE":  
-  case "leave":
+  case 3:
     window.alert("Leaving the store.");
     // do nothing, so function will end
     break;
